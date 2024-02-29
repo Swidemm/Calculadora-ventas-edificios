1eb4e7c1c72c0476b78b8c5442c78f8eaf4afb23
swidemm <lauti.seid@gmail.com>
2024-02-29 19:42:35 -0300

    finalizacion simulador

--- js/scripts.js
+++ js/scripts.js
@@ -47,38 +47,41 @@ while (Aventura == true) {
     }
     alert("Excelente eleccion, el tamaño total es de " + TamañoTotal + " " + "yo te recomendaria una casa " + RecomendacionCasa + " " +  "para aprovechar bien el espacio...");
 
-    alert("volviendo al terreno, este mismo tiene un valor de " + ValorTerreno + " " + "aunque si compras una casa prefabricada, te incluye un descuento por combo!");
+    alert("volviendo al terreno, este mismo tiene un valor de " + ValorTerreno + " " + "en mi opinion, te conviene comprar tanto el terreno como la casa prefabricada, ya que las tenemos en descuento!");
 
     alert("ahora, vamos por la casa, dime, ¿deseas una prefabricada o quisieras que me comunique contigo para hacer una a medida?") ;
 
     let TipoCasa = prompt("Me gustaria... \n1. Una casa prefabricada \n2. Que te comuniques conmigo para hacer una a medida");
 
     if (TipoCasa == 1) {
-        Aventura2 = true
+
+        let CasaPrefab = prompt("Genial, por favor, selecciona cual deseas (1, 2 o 3) \n1. Prefabricada de lujo \n2. Prefabricada clase media \n3. Prefabricada basica")
+        if (CasaPrefab == 1) {
+            ValorCasa = 12000;
+            TipoCasaPrefab = "de lujo"
+        } else if (CasaPrefab == 2) {
+            ValorCasa = 9500;
+            TipoCasaPrefab = "clase media"
+        } else if (CasaPrefab == 3) {
+            ValorCasa = 7200;
+            TipoCasaPrefab = "basica"
+        }
+        let Total = ValorCasa + ValorTerreno
+        alert("Asi que una casa " + TipoCasaPrefab + " " + "eh? excelente eleccion! esa misma vale " + ValorCasa + ".")
+        alert("Excelente, entonces seria... " + Total + ".")
+        alert("Respondes: Genial entonces! ¿Para cuando estaria todo esto?")ç
+        alert("Para aproximadamente 7 dias habiles desde que pagas")
+        let ModoPago = prompt("Pago con... \n1. efectivo \n2. debito \n1. credito ")
+        let NumeroCelularTotal = prompt("excelente eleccion, ya mismo te cobro... ¡y listo! Ya es todo tuyo, en los proximos dias habiles me voy a estar comunicando para aclarar los detalles, por favor, dejame tu numero de telefono...")
+        alert("Excelente, entonces tu numero es " + NumeroCelular + " " + "¿cierto? Muy bien, me comunicare contigo en los proximos 7 dias habiles, ¡Que te vaya mas que bien!");
+        alert("Sales del local, con una sonrisa por la gran atencion y precios, luego de unas cuadras, llegas a tu departamento, ilusionado de haber logrado tener tu propia casa, te acuestas y terminas el dia.")
         break;
+
     } else if (TipoCasa == 2) {
         alert("Genial, por favor anotame tu numero de celular en esta libreta asi me comunico");
         let NumeroCelular = prompt ("Agarras la libreta y escribes tu numero de celular...");
         alert("Excelente, entonces tu numero es " + NumeroCelular + " " + "¿cierto? Muy bien, me comunicare contigo en las proximas 24 horas, ¡Que te vaya mas que bien!");
-        alert("Sales del local, con una sonrisa por la gran atencion y precios, luego de unas cuadras, llegas a tu casa y terminas el dia.")
+        alert("Sales del local, con una sonrisa por la gran atencion y precios, luego de unas cuadras, llegas a tu departamento y terminas el dia.")
         break;
     }
-}
-
-while (Aventura2 == true) {
-    
-    alert("Genial al comprar una prefabricada, el total tiene un descuento del 10%! por favor, selecciona la casa...")
-    let CasaPrefab = prompt("Por favor, selecciona cual deseas (1, 2 o 3) \n1. Prefabricada de lujo \n2. Prefabricada clase media \n3. Prefabricada basica")
-    if (CasaPrefab == 1) {
-        ValorCasa = 12000;
-        TipoCasaPrefab = "de lujo"
-    } else if (CasaPrefab == 2) {
-        ValorCasa = 9500;
-        TipoCasaPrefab = "clase media"
-    } else if (CasaPrefab == 3) {
-        ValorCasa = 7200;
-        TipoCasaPrefab = "basica"
-    }
-    
-    alert("Asi que una casa " + TipoCasaPrefab + "" + "eh? excelente eleccion! esa misma vale " + ValorCasa + ".")
 }
